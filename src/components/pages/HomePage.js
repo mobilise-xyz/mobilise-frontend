714876a6453b7e51f2596b58444e31a4d19509d4
Joon-Ho Son <json0820@gmail.com>
2019-05-24 13:02:36 +0100

    Add linting and HTTP request testing.

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class HomePage extends React.Component {
+
+    componentDidMount() {
+        const response = axios.get('/users');
+        console.log(response);
+    }
+
     render() {
         return (
             <div>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import axios from 'axios';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+// Axios configuration
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>,
-    document.getElementById('root'));
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
