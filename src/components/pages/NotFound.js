535ac0da331b43d1ae61f0436199cf0be4ef362c
Joon-Ho Son <json0820@gmail.com>
2019-05-25 23:03:27 +0100

    Add basic 404 page.

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Row, Col, Image } from 'react-bootstrap';
+import logo from '../logo.png';
+import Layout from '../Layout';
+
+const NotFound = () => (
+  <Layout>
+    <Row>
+      <Col style={{ 'text-align': 'center' }}>
+        <Image src={logo} alt="Mobilise logo" fluid />
+      </Col>
+    </Row>
+    <Row className="pt-5">
+      <Col style={{ 'text-align': 'center' }}>
+        This is not the page you are looking for.
+      </Col>
+    </Row>
+  </Layout>
+);
+
+export default NotFound;
