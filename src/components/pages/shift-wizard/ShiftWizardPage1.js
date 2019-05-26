e932be66328a0dc573a092ae0d11fce8115f9cd6
Tiger Cross <tc2017@ic.ac.uk>
2019-05-26 15:23:59 +0100

    Add shift wizard page and card columns.

--- src/components/pages/shift-wizard/ShiftWizardPage1.js
+++ src/components/pages/shift-wizard/ShiftWizardPage1.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { CardColumns, Card } from 'react-bootstrap';
 import Layout from '../../Layout';
 
-const ShiftWizardPage1 = () => <Layout>Shift</Layout>;
+const ShiftWizardPage1 = () => (
+  <Layout>
+    <CardColumns>
+      <Card style={{ width: '60%' }}>
+        <Card.Body>
+          <Card.Title>Shift Wizard</Card.Title>
+        </Card.Body>
+      </Card>
+    </CardColumns>
+  </Layout>
+);
 
 export default ShiftWizardPage1;
