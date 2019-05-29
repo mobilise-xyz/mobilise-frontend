4ec92c530a8440771eca710d21b11cf3f351fa83
Tiger Cross <tc2017@ic.ac.uk>
2019-05-29 01:42:23 +0100

    Use css file for Rolesform.

--- src/components/forms/RolesForm.js
+++ src/components/forms/RolesForm.js
@@ -1,32 +1,17 @@
 import React from 'react';
 import { Form, Row, Col } from 'react-bootstrap';
 import { Typeahead, Token } from 'react-bootstrap-typeahead';
+import './RolesForm.css';
 
 class RolesForm extends React.Component {
   _renderToken = (option, props, index) => (
     <Token key={index} style={{}} onRemove={props.onRemove}>
       <Row>
-        <Col
-          md="auto"
-          style={{
-            padding: '0 0.2rem 0 1rem',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center'
-          }}
-        >
+        <Col md="auto" className="role-name">
           {option.label ? option.label : option}
         </Col>
-        <Col
-          style={{
-            padding: '0 1rem 0 0.2rem',
-            width: '3.2rem'
-          }}
-        >
-          <Form.Control
-            type="number"
-            style={{ height: '1.4rem', textAlign: 'center' }}
-          />
+        <Col className="role-num-col">
+          <Form.Control type="number" className="role-num-form" />
         </Col>
       </Row>
     </Token>
@@ -37,7 +22,7 @@ class RolesForm extends React.Component {
     return (
       <Form.Group controlId="rolesForm">
         <Form.Label>Roles</Form.Label>
-        <Typeahead // TODO make async & SORT OUT CSS so letters like g dont get cut off.
+        <Typeahead // TODO make async & SORT OUT CSS so letters like g doesn't get cut off.
           renderToken={this._renderToken}
           id="roles"
           placeholder="Add available roles for shift"
