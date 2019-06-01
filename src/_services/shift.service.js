6c1b312ac2e4f8a2913c2a121b63389ef88331bc
Joon-Ho Son <json0820@gmail.com>
2019-06-01 16:43:47 +0100

    Add shift service to book a shift.

--- src/_services/shift.service.js
+++ src/_services/shift.service.js
@@ -7,13 +7,30 @@ const deleteWithId = shiftId => {
     headers: authHeader()
   };
 
-  console.log('Deleting shift');
+  console.log('Deleting shift with ID', shiftId);
 
   return axios.delete(`/shifts/${shiftId}`, config).then(utils.handleResponse);
 };
 
+const bookWithIdAndRole = (shiftId, roleName) => {
+  const config = {
+    headers: authHeader()
+  };
+
+  const data = {
+    roleName
+  };
+
+  console.log('Booking shift with ID', shiftId);
+
+  return axios
+    .post(`shifts/${shiftId}/book`, config, data)
+    .then(utils.handleResponse);
+};
+
 const shiftService = {
-  deleteWithId
+  deleteWithId,
+  bookWithIdAndRole
 };
 
 export default shiftService;
