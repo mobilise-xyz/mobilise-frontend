ecd55806eeb8457daabd6c3668e0915c088d64fe
Joon-Ho Son <json0820@gmail.com>
2019-06-02 17:09:52 +0100

    Add reducer for deletion.

--- src/_reducers/shifts.reducer.js
+++ src/_reducers/shifts.reducer.js
@@ -1,5 +1,7 @@
 import shiftsConstants from '../_constants/shifts.constants';
 
+// Utility function that applies a function to all and recommended shifts and
+// returns the appropriate state.
 const applyToShifts = (shifts, action, f) => ({
   all: shifts.all.map(f),
   recommended: shifts.recommended.map(f)
@@ -43,21 +45,42 @@ const shifts = (state = initialState, action) => {
       return {
         error: action.error
       };
+    case shiftsConstants.DELETE_REQUEST: {
+      const setDeleteRequest = shift =>
+        shift.id === action.id ? { ...shift, loading: true } : shift;
+      return { shifts: applyToShifts(state.shifts, action, setDeleteRequest) };
+    }
+    case shiftsConstants.DELETE_SUCCESS: {
+      const setBookSuccess = shift =>
+        shift.id === action.id
+          ? { ...shift, deleteSuccess: true, loading: false }
+          : shift;
+      return { shifts: applyToShifts(state.shifts, action, setBookSuccess) };
+    }
+    case shiftsConstants.DELETE_FAILURE: {
+      const setBookFailure = shift =>
+        shift.id === action.id
+          ? {
+              ...shift,
+              deleteSccess: false,
+              loading: false,
+              error: action.error
+            }
+          : shift;
+      return { shifts: applyToShifts(state.shifts, action, setBookFailure) };
+    }
     case shiftsConstants.BOOK_REQUEST: {
       // Search for the shift that requested to be booked.
       const setBookRequest = shift =>
         shift.id === action.id ? { ...shift, loading: true } : shift;
-      console.log('STATE SHIFTS', state.shifts);
       return { shifts: applyToShifts(state.shifts, action, setBookRequest) };
     }
     case shiftsConstants.BOOK_SUCCESS: {
       // Search for the shift that requested to be booked.
-
       const setBookSuccess = shift =>
         shift.id === action.id
           ? { ...shift, bookSuccess: true, loading: false }
           : shift;
-
       return { shifts: applyToShifts(state.shifts, action, setBookSuccess) };
     }
     case shiftsConstants.BOOK_FAILURE: {
