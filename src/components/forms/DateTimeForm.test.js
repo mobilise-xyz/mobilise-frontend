46a8d38432e8056f4181ced107252cd06e250812
Tiger Cross <tc2017@ic.ac.uk>
2019-06-03 16:39:00 +0100

    Add more specific component tests for DateTimeForm

--- src/components/forms/DateTimeForm.test.js
+++ src/components/forms/DateTimeForm.test.js
@@ -12,4 +12,20 @@ describe('Date and Time Form Component', () => {
   it('renders form correctly', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('contains date selector', () => {
+    expect(wrapper.find('FormControl[type="date"]')).toHaveLength(1);
+  });
+
+  it('contains start time selector', () => {
+    expect(
+      wrapper.find('FormControl[id="start-time"][type="time"]')
+    ).toHaveLength(1);
+  });
+
+  it('contains end time selector', () => {
+    expect(
+      wrapper.find('FormControl[id="end-time"][type="time"]')
+    ).toHaveLength(1);
+  });
 });
