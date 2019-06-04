4f2231faedd884591f636fa6717ca79ff156f8a7
Joon-Ho Son <json0820@gmail.com>
2019-06-04 15:57:38 +0100

    Clear alerts on location change.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import { Switch, Route, Redirect } from 'react-router';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect, Route, Switch } from 'react-router';
+import history from './_helpers/history';
 import PrivateRoute from './components/PrivateRoute';
 import ShiftsPage from './components/pages/ShiftsPage';
 import MyShiftsPage from './components/pages/MyShiftsPage';
@@ -9,19 +11,41 @@ import NotFound from './components/pages/NotFound';
 import SettingsPage from './components/pages/SettingsPage';
 import SignUpPage from './components/pages/SignUpPage';
 import AvailabilityPage from './components/pages/AvailabilityPage';
+import alertActions from './_actions/alert.actions';
 
-const App = () => (
-  <Switch>
-    <PrivateRoute path="/" exact component={ShiftsPage} />
-    <PrivateRoute path="/shifts" exact component={MyShiftsPage} />
-    <PrivateRoute path="/new-shift" exact component={NewShiftPage} />
-    <PrivateRoute path="/settings" exact component={SettingsPage} />
-    <PrivateRoute path="/availability" exact component={AvailabilityPage} />
-    <Route path="/login" exact component={LoginPage} />
-    <Route path="/signup" exact component={SignUpPage} />
-    <Route path="/404" exact component={NotFound} />
-    <Redirect from="*" to="/404" />
-  </Switch>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const { dispatch } = this.props;
 
-export default App;
+    // Clear alerts on location change.
+    history.listen(() => {
+      dispatch(alertActions.clear());
+    });
+  }
+
+  render() {
+    return (
+      <Switch>
+        <PrivateRoute path="/" exact component={ShiftsPage} />
+        <PrivateRoute path="/shifts" exact component={MyShiftsPage} />
+        <PrivateRoute path="/new-shift" exact component={NewShiftPage} />
+        <PrivateRoute path="/settings" exact component={SettingsPage} />
+        <PrivateRoute path="/availability" exact component={AvailabilityPage} />
+        <Route path="/login" exact component={LoginPage} />
+        <Route path="/signup" exact component={SignUpPage} />
+        <Route path="/404" exact component={NotFound} />
+        <Redirect from="*" to="/404" />
+      </Switch>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  const { alert } = state;
+  return {
+    alert
+  };
+};
+
+export default connect(mapStateToProps)(App);
