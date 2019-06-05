e06883b5d3e596913c9e146b68231034210e11ef
Joon-Ho Son <json0820@gmail.com>
2019-06-05 18:47:28 +0100

    Add icons to activity.

--- src/components/pages/Dashboard/MyActivity/MyActivity.js
+++ src/components/pages/Dashboard/MyActivity/MyActivity.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import { Card, Container, ListGroup } from 'react-bootstrap';
+import { Card, Col, Container, ListGroup, Row } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShareAlt } from '@fortawesome/free-solid-svg-icons';
+import { faShareAlt, faTrophy } from '@fortawesome/free-solid-svg-icons';
 
 const placeholderActivity = [
+  {
+    title: 'Completed your first shift!',
+    description: 'You volunteered for Tesco Pickup.'
+  },
+  {
+    title: 'Completed your first shift!',
+    description: 'You volunteered for Tesco Pickup.'
+  },
+  {
+    title: 'Completed your first shift!',
+    description: 'You volunteered for Tesco Pickup.'
+  },
+  {
+    title: 'Completed your first shift!',
+    description: 'You volunteered for Tesco Pickup.'
+  },
+  {
+    title: 'Completed your first shift!',
+    description: 'You volunteered for Tesco Pickup.'
+  },
+  {
+    title: 'Completed your first shift!',
+    description: 'You volunteered for Tesco Pickup.'
+  },
+  {
+    title: 'Completed your first shift!',
+    description: 'You volunteered for Tesco Pickup.'
+  },
   {
     title: 'Completed your first shift!',
     description: 'You volunteered for Tesco Pickup.'
@@ -13,17 +41,31 @@ const placeholderActivity = [
 
 const ActivityCard = ({ title, description }) => (
   <Card className="mt-1">
-    <Card.Body>
-      <Card.Title>{title}</Card.Title>
-      <Card.Text>
-        {description}{' '}
-        <div id="share" style={{ textAlign: 'right' }}>
+    <Card.Body className="h-100 align-items-center">
+      <Row className="h-100">
+        <Col md={1} className="h-100" style={{ margin: 'auto' }}>
+          <FontAwesomeIcon icon={faTrophy} size="3x" className="text-primary" />
+        </Col>
+        <Col>
+          <Card.Title>{title}</Card.Title>
+          <Card.Body style={{ padding: 0 }}>{description}</Card.Body>
+        </Col>
+        <Col md={1} style={{ marginTop: 'auto' }}>
           <FontAwesomeIcon icon={faShareAlt} />
-        </div>
-      </Card.Text>
+        </Col>
+      </Row>
     </Card.Body>
   </Card>
 );
+//
+// {/*<FontAwesomeIcon icon={faTrophy} />*/}
+//
+// {/*<Card.Body>*/}
+// {/*<Card.Title>{title}</Card.Title>*/}
+// {/*<Card.Text>*/}
+// {/*{description}*/}
+// {/**/}
+// {/*</Card.Text>*/}
 
 ActivityCard.propTypes = {
   title: PropTypes.string.isRequired,
