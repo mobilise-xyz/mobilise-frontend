c37d81d7e4e9a803ac8ea0b973258b44b2d91d4c
Joon-Ho Son <json0820@gmail.com>
2019-06-13 12:45:26 +0100

    Add container option for CardLayout.

--- src/components/CardLayout.js
+++ src/components/CardLayout.js
@@ -1,21 +1,48 @@
 import React from 'react';
-import { Card, Col, Row } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import Layout from './Layout';
 
-const CardLayout = ({ title, navOff, backBtn, children }) => (
-  <Layout navOff={navOff}>
-    <Row>
-      <Col>{backBtn}</Col>
-    </Row>
+const CardLayout = ({
+  title,
+  navOff,
+  backButton,
+  children,
+  container = false
+}) => {
+  const backBtn = backButton ? (
     <Row>
       <Col>
-        <Card.Title>{title}</Card.Title>
-        <Card>
-          <Card.Body>{children}</Card.Body>
-        </Card>
+        <LinkContainer exact to="/" className="float-right">
+          <Button variant="outline-secondary">
+            {<FontAwesomeIcon icon={faAngleLeft} />} Back
+          </Button>
+        </LinkContainer>
       </Col>
     </Row>
-  </Layout>
-);
+  ) : null;
+
+  const inner = (
+    <>
+      {backBtn}
+      <Row>
+        <Col>
+          <Card.Title>{title}</Card.Title>
+          <Card>
+            <Card.Body>{children}</Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+
+  return (
+    <Layout navOff={navOff}>
+      {container ? <Container>{inner}</Container> : inner}
+    </Layout>
+  );
+};
 
 export default CardLayout;
