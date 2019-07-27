042a8ca3ef0aa59d49ffdbd29e6a4647f126fd62
Joon-Ho Son <json0820@gmail.com>
2019-07-27 17:11:08 +0100

    Basic feedback form.

--- src/components/pages/HelpPages/FeedbackPage.js
+++ src/components/pages/HelpPages/FeedbackPage.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+
 import CardLayout from '../../CardLayout';
 
-const FeedBackPage = () => (
-  <CardLayout title="Feedback">Give feedback!</CardLayout>
+const FeedbackPage = () => (
+  <CardLayout title="Feedback">
+    <p>Your feedback is valuable to</p>
+    <Form>
+      <Form.Group>
+        <Form.Label>Enter your message below</Form.Label>
+        <Form.Control as="textarea" rows="30" />
+      </Form.Group>
+      <Button type="submit" className="btn-more-info">
+        Send
+      </Button>
+    </Form>
+  </CardLayout>
 );
 
-export default FeedBackPage;
+export default FeedbackPage;
