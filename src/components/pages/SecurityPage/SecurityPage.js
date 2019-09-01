e75d770dab3224a79cc26aab6168d7f6404589ec
Arjun Singh <arjun_singh2@apple.com>
2019-09-01 17:01:07 +0100

    Add Security Page with Change Password feature

---
+++ src/components/pages/SecurityPage/SecurityPage.js
@@ -0,0 +1,19 @@
+import Layout from '../../Layout';
+import { Container } from 'react-bootstrap';
+import ErrorBoundary from '../../ErrorBoundary';
+import ChangePasswordForm from '../../forms/ChangePasswordForm';
+
+const SecurityPage = () => {
+    return (
+        <Layout>
+            <Container>
+                <h3 className="pt-5">Change Password</h3>
+                <ErrorBoundary>
+                    <ChangePasswordForm />
+                </ErrorBoundary>
+            </Container>
+        </Layout>
+    );
+}
+
+export default SecurityPage;
