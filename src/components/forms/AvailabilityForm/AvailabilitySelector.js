71b7b6322c83642d1b01fbf0c7a37f79a94de5d1
Will Burr <williamburr98@gmail.com>
2019-09-12 14:07:44 +0100

    Separate out the availability grid from the selector

--- src/components/forms/AvailabilityForm/AvailabilitySelector.js
+++ src/components/forms/AvailabilityForm/AvailabilitySelector.js
@@ -1,46 +1,10 @@
 import React from 'react';
-import { Card, Col, Container, Row, Table } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import availabilityConstants from '../../../_constants/availability.constants';
 import '../../ShiftCard/ShiftCard.css';
+import AvailabilityGrid from './AvailabilityGrid';
 import availabilityActions from '../../../_actions/availability.actions';
-
-const times = ['Morning', 'Afternoon', 'Evening'];
-const timesInfo = ['6AM - 12PM', '12PM - 5PM', '5PM-11PM'];
-
-const days = [
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-  'Sunday'
-];
-
-const availabilityColours = {
-  AVAILABILITY_AVAILABLE: '#27a659',
-  AVAILABILITY_MAYBE: 'Yellow',
-  AVAILABILITY_UNAVAILABLE: 'LightGray'
-};
-
-const DayCard = ({
-  timeIndex,
-  dayIndex,
-  availabilityConstant,
-  handleClick
-}) => (
-  <Card style={{ backgroundColor: availabilityColours[availabilityConstant] }}>
-    <Card.Body />
-    <button
-      type="button"
-      className="stretched-link shift-card-btn"
-      onClick={() => handleClick(timeIndex, dayIndex, availabilityConstant)}
-    >
-      <span className="sr-only">Availability button</span>
-    </button>
-  </Card>
-);
 
 const miniCardStyle = colour => ({
   width: '1rem',
@@ -87,56 +51,6 @@ class AvailabilitySelector extends React.Component {
     }
   };
 
-  generateGrid = availability => {
-    const grid = [
-      <thead key="grid-days-header">
-        <tr key="grid-days-header-row">
-          <th style={{ width: '12.5%' }} />
-          {days.map((day, dayIndex) => (
-            <th
-              style={{
-                width: '12.5%',
-                fontStyle: 'unset',
-                color: 'unset',
-                fontWeight: 'unset',
-                textAlign: 'center'
-              }}
-              key={`grid-${days[dayIndex].toLowerCase()}-header`}
-            >
-              {days[dayIndex]}
-            </th>
-          ))}
-        </tr>
-      </thead>
-    ];
-
-    grid.push(
-      <tbody key="grid-days-body  ">
-        {availability.map((time, timeIndex) => (
-          <tr key={`grid-${times[timeIndex].toLowerCase()}`} className="pt-4">
-            <td>
-              <Row>{times[timeIndex]}</Row>
-              <Row style={{ color: 'gray' }}>{timesInfo[timeIndex]}</Row>
-            </td>
-            {time.map((day, dayIndex) => (
-              <td key={`grid-${days[dayIndex].toLowerCase()}`}>
-                <DayCard
-                  key={`${times[timeIndex]}-${days[dayIndex]}`}
-                  timeIndex={timeIndex}
-                  dayIndex={dayIndex}
-                  availabilityConstant={day}
-                  handleClick={this.handleClick}
-                />
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    );
-
-    return <Table>{grid}</Table>;
-  };
-
   render() {
     const { availability } = this.props;
     return (
@@ -172,7 +86,10 @@ class AvailabilitySelector extends React.Component {
           </Col>
         </Row>
         <Container className="table-responsive">
-          {this.generateGrid(availability)}
+          <AvailabilityGrid
+            availability={availability}
+            handleClick={this.handleClick}
+          />
         </Container>
       </>
     );
